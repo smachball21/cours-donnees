b03a2dbdd497332f7d6107597e6b37c8d5c4f5c7
Webmaster Rayformatics <webmaster@rayformatics.fr>
2020-11-09 13:24:51 +0100

    master

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', 'AdminController@index');
+Route::get('/superadmin', 'SuperAdminController@index');
